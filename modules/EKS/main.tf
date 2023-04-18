3660b6e2182a875507b502d584ac4ac797d1e3ba
Aravindkv5 <aravindkarthi5@gmail.com>
2023-04-18 23:06:12 +0530

    change the module side of EKS

--- modules/EKS/main.tf
+++ modules/EKS/main.tf
@@ -1,21 +1,21 @@
 
 
-resource "aws_iam_role_policy_attachment" "AmazonEKSClusterPolicy" {
-    policy_arn  = "arn:aws:iam::691575244851:role/EKS-Cluster"
-    role = aws_iam_role.eks_cluster.name
+# resource "aws_iam_role_policy_attachment" "AmazonEKSClusterPolicy" {
+#     policy_arn  = "arn:aws:iam::691575244851:role/EKS-Cluster"
+#     role = aws_iam_role.eks_cluster.name
 
   
-}
+# }
 
-resource "aws_iam_role_policy_attachment" "AmazonEKSServicePolicy" {
-    policy_arn  = "arn:aws:iam::691575244851:role/aws-service-role/eks.amazonaws.com/AWSServiceRoleForAmazonEKS"
-    role = aws_iam_role.eks_cluster.name
+# resource "aws_iam_role_policy_attachment" "AmazonEKSServicePolicy" {
+#     policy_arn  = "arn:aws:iam::691575244851:role/aws-service-role/eks.amazonaws.com/AWSServiceRoleForAmazonEKS"
+#     role = aws_iam_role.eks_cluster.name
   
-}
+# }
 
 resource "aws_eks_cluster" "aws_eks" {
-    name = aws_iam_role.eks_cluster.name
-    role_arn = aws_iam_role.eks_cluster.arn
+    name = "Tes-Demo"
+    /* role_arn = aws_iam_role.eks_cluster.arn */
 
     vpc_config {
         subnet_ids = ["subnet-0925b04d79caa625a","subnet-0adf449365f26d140"]
@@ -31,14 +31,14 @@ resource "aws_eks_cluster" "aws_eks" {
 
 resource "aws_iam_policy_attachment" "AmazonEKSWorkerNodePolicy" {
     policy_arn = "arn:aws:iam::691575244851:role/aws-service-role/eks-nodegroup.amazonaws.com/AWSServiceRoleForAmazonEKSNodegroup"
-    roles = aws_iam_role.eks_nodes.name
-    name = aws_iam_role.eks_cluster.name
+    
+    name = "test"
   
 }
 
 resource "aws_iam_role_policy_attachment" "AmazonEKS_CNI_Policy" {
     policy_arn = "arn:aws:iam::691575244851:role/EKS"
-    role = aws_iam_role.eks_nodes.name
+    role = "test"
   
 }
 
