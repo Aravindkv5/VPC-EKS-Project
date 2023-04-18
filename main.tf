e3b2bb8b11e322ebbcba17fa13ae81889cfa6914
Aravindkv5 <aravindkarthi5@gmail.com>
2023-04-18 22:58:20 +0530

    changed the main.tf for  EKS

--- main.tf
+++ main.tf
@@ -31,8 +31,8 @@ module "create_ap-south-2" {
 module "eks"{
   source = "./modules/EKS"
   cluster_name =  "test"
-  aws_iam_role = "trial"
-  aws_eks_node = "EKS"
+  #aws_iam_role = "trial"
+  #aws_eks_node = "EKS"
   
 
 
