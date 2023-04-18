21c523562da86eecbe511e675eeac60f6fe83ede
Aravindkv5 <aravindkarthi5@gmail.com>
2023-04-18 23:01:11 +0530

    change the module side of EKS

--- modules/EKS/main.tf
+++ modules/EKS/main.tf
@@ -1,27 +1,4 @@
-resource "aws_iam_role" "eks_cluster" {
-    name = var.cluster_name
 
-    tags = {
-        Name = var.cluster_name
-    }
-  assume_role_policy = <<POLICY
-  {
-    "version" : "2023-04-16",
-    "Statement": [
-        {
-            "Effect": "Allow"
-            "Principle" : {
-                "Service : "eks.amazon.com"
-            },
-            "Action" : "sts:AssumeRole"
-            
-        }
-    ]
-  }
-  POLICY
-
-  
-}
 
 resource "aws_iam_role_policy_attachment" "AmazonEKSClusterPolicy" {
     policy_arn  = "arn:aws:iam::691575244851:role/EKS-Cluster"
@@ -65,19 +42,15 @@ resource "aws_iam_role_policy_attachment" "AmazonEKS_CNI_Policy" {
   
 }
 
-resource "aws_iam_role_policy_attachment" "AmazonEC2ContainerRegistryReadOnly" {
-    policy_arn = "arn:aws:iam::691575244851:role/aws-service-role/replication.ecr.amazonaws.com/AWSServiceRoleForECRReplication"
-    
-  
-}
+
 
 resource "aws_eks_node_group" "node" {
     cluster_name = aws_eks_cluster.aws_eks.name
-    node_group_name = aws_iam_role.eks_nodes.Name
-    node_role_arn = aws_iam_role.eks_nodes.role_arn
+    node_group_name = "DEMO-Test"
+    #node_role_arn = aws_iam_role.eks_nodes.role_arn
     subnets_ids = ["subnet-0925b04d79caa625a","subnet-0adf449365f26d140"]
     tags = {
-        Name = aws_iam_role.eks_cluster.cluster_name
+        Name = "Mohanram"
     }
   
   scaling_config {
