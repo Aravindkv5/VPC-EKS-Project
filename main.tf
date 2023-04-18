56594469a78c54a7b3b9b4557d8bb3760620d81a
Aravindkv5 <aravindkarthi5@gmail.com>
2023-04-18 21:22:17 +0530

    edited STAGE

--- main.tf
+++ main.tf
@@ -20,7 +20,7 @@ module "create_ap-south-2" {
   public_subnets  = var.public_subnets
   private_subnets = var.private_subnets
   DEFAULT_TAGS    = var.DEFAULT_TAGS
-  STAGE           = env
+  STAGE           = var.STAGE
     
   providers = {
     aws.vpc_region = aws.Hyderabad
