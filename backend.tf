958fcd50d9b611cde2e1144cc7c50963c3adf9ff
Aravindkv5 <aravindkarthi5@gmail.com>
2023-04-22 21:29:05 +0530

    added s3 and dynamodb hardcoded for backend

--- backend.tf
+++ backend.tf
@@ -1,8 +1,36 @@
+resource "aws_s3_bucket" "bucket" {
+    bucket = "angelo-terraform-state-backend"
+    versioning {
+        enabled = true
+    }
+    
+    object_lock_configuration {
+        object_lock_enabled = "Enabled"
+    }
+    tags = {
+        Name = "S3 Remote Terraform State Store"
+    }
+}
+
+resource "aws_dynamodb_table" "terraform-lock" {
+    name           = "terraform_state"
+    read_capacity  = 5
+    write_capacity = 5
+    hash_key       = "LockID"
+    attribute {
+        name = "LockID"
+        type = "S"
+    }
+    tags = {
+        "Name" = "DynamoDB Terraform State Lock Table"
+    }
+}
+
 terraform {
   backend "s3" {
-    bucket = "new-s3-bucket-terraform"
-    key = "terraform"
-    region = "ap-south-1"
-    dynamodb_table = "terraform-lock"
+    bucket         = "aterraform-state-backend"
+    key            = "terraform.tfstate"
+    region         = "ap-south-1"
+    dynamodb_table = "terraform_state"
   }
 }
