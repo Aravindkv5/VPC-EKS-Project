2d8de6e2388160f309c3081f91791d9cbfda770a
Aravindkv5 <aravindkarthi5@gmail.com>
2023-04-22 20:26:59 +0530

    Added tfvars

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ module "create_ap-south-1" {
   public_subnets  = var.public_subnets
   private_subnets = var.private_subnets
   DEFAULT_TAGS    = var.DEFAULT_TAGS
-  STAGE           = "Environment"
+  Environment     = var.Environment
     
   providers = {
     aws.vpc_region = aws.Mumbai
@@ -20,7 +20,7 @@ module "create_ap-south-2" {
   public_subnets  = var.public_subnets
   private_subnets = var.private_subnets
   DEFAULT_TAGS    = var.DEFAULT_TAGS
-  STAGE           = "Environment"
+  Environment     = var.Environment
     
   providers = {
     aws.vpc_region = aws.Hyderabad
