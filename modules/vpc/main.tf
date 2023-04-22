76eee2ae6cbf59f08009d772c315e987093ada7e
Aravindkv5 <aravindkarthi5@gmail.com>
2023-04-22 20:37:10 +0530

    Added tags to VPC and changed the tfvars

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -14,17 +14,19 @@ data "aws_region" "current" {
 resource "aws_vpc" "main" {
   provider   = aws.vpc_region
   cidr_block = lookup(var.cidr_block, data.aws_region.current.name)
-  
+  tags       = merge(tomap({ "Name" : "Terraform-vpc" }), tomap({ "Created by" : "Aravind" }), var.DEFAULT_TAGS)
 }
 
 resource "aws_internet_gateway" "vpc_gateway" {
   provider = aws.vpc_region
   vpc_id   = aws_vpc.main.id
+  tags     = merge(tomap({ "Name" : "Terraform-igw" }), tomap({ "Created by" : "Aravind" }), var.DEFAULT_TAGS)
 }
 
 resource "aws_route_table" "main_routetable" {
   provider = aws.vpc_region
   vpc_id   = aws_vpc.main.id
+  tags     = merge(tomap({ "Name" : "Terraform-main-rt" }), tomap({ "Created by" : "Aravind" }), var.DEFAULT_TAGS)
 }
 
 resource "aws_main_route_table_association" "route_table_association" {
@@ -39,6 +41,7 @@ resource "aws_subnet" "public_subnet" {
   provider   = aws.vpc_region
   vpc_id     = aws_vpc.main.id
   cidr_block = lookup(var.public_subnets, data.aws_region.current.name)[count.index]
+  tags       = merge(tomap({ "Name" : "Terraform-private-${count.index + 1}" }), tomap({ "Created by" : "Aravind" }), var.DEFAULT_TAGS)
 }
 
 resource "aws_route_table" "public_route_table" {
@@ -48,6 +51,7 @@ resource "aws_route_table" "public_route_table" {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.vpc_gateway.id
   }
+  tags = merge(tomap({ "Name" : "Terraform-public-rt" }), tomap({ "Created by" : "Aravind" }), var.DEFAULT_TAGS)
 }
 
 resource "aws_route_table_association" "public" {
@@ -62,19 +66,21 @@ resource "aws_subnet" "private_subnet" {
   provider   = aws.vpc_region
   vpc_id     = aws_vpc.main.id
   cidr_block = lookup(var.private_subnets, data.aws_region.current.name)[count.index]
+  tags       = merge(tomap({ "Name" : "Terraform-private-${count.index + 1}" }), tomap({ "Created by" : "Aravind" }), var.DEFAULT_TAGS)
 }
 
 resource "aws_eip" "eip_nat_gateway" {
   provider         = aws.vpc_region
   vpc              = true
   public_ipv4_pool = "amazon"
+  tags             = merge(tomap({ "Name" : "Terraform-nat-eip" }), tomap({ "Created by" : "Aravind" }), var.DEFAULT_TAGS)
 }
 
 resource "aws_nat_gateway" "nat_gateway" {
   provider      = aws.vpc_region
   allocation_id = aws_eip.eip_nat_gateway.id
   subnet_id     = aws_subnet.public_subnet[0].id
-  tags          = merge(tomap({ "Name" : "decode-platform-nat-gw" }), tomap({ "STAGE" : var.STAGE }), var.DEFAULT_TAGS)
+  tags          = merge(tomap({ "Name" : "Terraform-nat-gw" }), tomap({ "Created by" : "Aravind" }), var.DEFAULT_TAGS)
   depends_on    = [aws_internet_gateway.vpc_gateway, aws_eip.eip_nat_gateway]
 }
 
@@ -85,6 +91,7 @@ resource "aws_route_table" "private_route_table" {
     cidr_block     = "0.0.0.0/0"
     nat_gateway_id = aws_nat_gateway.nat_gateway.id
   }
+  tags = merge(tomap({ "Name" : "Terraform-private-rt" }), tomap({ "Created by" : "Aravind" }), var.DEFAULT_TAGS)
 }
 
 resource "aws_route_table_association" "private" {
@@ -94,3 +101,20 @@ resource "aws_route_table_association" "private" {
   route_table_id = aws_route_table.private_route_table.id
 }
 
+resource "aws_vpc_endpoint" "dynamodb_endpoint" {
+  provider          = aws.vpc_region
+  vpc_id            = aws_vpc.main.id
+  service_name      = "com.amazonaws.${data.aws_region.current.name}.dynamodb"
+  vpc_endpoint_type = "Gateway"
+  route_table_ids   = [aws_route_table.private_route_table.id]
+  tags              = merge(tomap({ "Name" : "Terraform-ddb-gateway-endpoint" }), tomap({ "Created by" : "Aravind" }), var.DEFAULT_TAGS)
+}
+
+resource "aws_vpc_endpoint" "s3_endpoint" {
+  provider          = aws.vpc_region
+  vpc_id            = aws_vpc.main.id
+  service_name      = "com.amazonaws.${data.aws_region.current.name}.s3"
+  vpc_endpoint_type = "Gateway"
+  route_table_ids   = [aws_route_table.private_route_table.id]
+  tags              = merge(tomap({ "Name" : "Terraform-s3-gateway-endpoint" }), tomap({ "Created by" : "Aravind" }), var.DEFAULT_TAGS)
+}
